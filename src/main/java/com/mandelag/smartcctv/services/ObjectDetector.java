5fcad9f27d68d670482557b6c9f894c30ec1db79
Keenan Gebze <keenan.gebze@gmail.com>
2018-05-22 02:52:34 +0700

    Embeding code from tensorflow objectdetection sample. https://github.com/tensorflow/models/tree/master/samples/languages/java/object_detection

--- src/main/java/com/mandelag/smartcctv/services/ObjectDetector.java
+++ src/main/java/com/mandelag/smartcctv/services/ObjectDetector.java
@@ -5,21 +5,30 @@
  */
 package com.mandelag.smartcctv.services;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import org.opencv.core.Mat;
 import org.tensorflow.Graph;
 import org.tensorflow.Operation;
 import org.tensorflow.Session;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.tensorflow.Tensor;
+import org.tensorflow.types.UInt8;
 
 /**
  *
@@ -27,11 +36,37 @@ import org.tensorflow.Session;
  */
 public class ObjectDetector {
 
+    private static void bgr2rgb(byte[] data) {
+        for (int i = 0; i < data.length; i += 3) {
+            byte tmp = data[i];
+            data[i] = data[i + 2];
+            data[i + 2] = tmp;
+        }
+    }
+
+    private static Tensor<UInt8> makeImageTensor(String filename) throws IOException {
+        BufferedImage img = ImageIO.read(new File(filename));
+        if (img.getType() != BufferedImage.TYPE_3BYTE_BGR) {
+            throw new IOException(
+                    String.format(
+                            "Expected 3-byte BGR encoding in BufferedImage, found %d (file: %s). This code could be made more robust",
+                            img.getType(), filename));
+        }
+        byte[] data = ((DataBufferByte) img.getData().getDataBuffer()).getData();
+        // ImageIO.read seems to produce BGR-encoded images, but the model expects RGB.
+        bgr2rgb(data);
+        final long BATCH_SIZE = 1;
+        final long CHANNELS = 3;
+        long[] shape = new long[]{BATCH_SIZE, img.getHeight(), img.getWidth(), CHANNELS};
+        return Tensor.create(UInt8.class, shape, ByteBuffer.wrap(data));
+    }
+
     public static void main(String[] args) {
         Graph objectDetectionGraph = new Graph();
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
         System.out.println("Current relative path is: " + s);
+
         try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("src\\main\\java\\res\\ssd_mobilenet_v1_coco_2017_11_17\\frozen_inference_graph.pb"))) {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             BufferedOutputStream bos = new BufferedOutputStream(baos);
@@ -50,7 +85,7 @@ public class ObjectDetector {
                 Operation ops = opsIterator.next();
                 try {
                     int length = ops.numOutputs();
-                    for (int i=0; i<length; i++) {
+                    for (int i = 0; i < length; i++) {
                         System.out.println(ops.output(i));
                     }
                 } catch (IllegalArgumentException e) {
